ea580b0314f50ef2cc0a926d48fda8646b7ea986
Jull-Saltykova <Saltykovajull@gmail.com>
2022-05-12 13:31:48 +0500

    Task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,14 +1,14 @@
 // Задание 4.1. Вывод карточек товаров
 
-function parseProducts(json) {
+
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
-function parseProducts(JSON) {
+    
+function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
 return products;
